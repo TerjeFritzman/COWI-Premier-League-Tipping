82c2d2ea71749c5a6d748c108cdbea0faa67710c
TerjeFritzman <terjefritz@hotmail.com>
2025-09-09 18:06:46 +0200

    predictions added

--- predictions.py
+++ predictions.py
@@ -1,56 +1,30 @@
 predictions_data = {
     "Terje": [
-        "Manchester City FC", "Sunderland AFC", "Tottenham Hotspur FC", "Liverpool FC", "Nottingham Forest FC",
-        "Arsenal FC", "Brighton & Hove Albion FC", "Fulham FC", "Aston Villa FC", "Chelsea FC",
-        "Crystal Palace FC", "Newcastle United FC", "Everton FC", "Leeds United FC", "Manchester United FC",
-        "AFC Bournemouth", "Brentford FC", "Burnley FC", "West Ham United FC", "Wolverhampton Wanderers FC"
-    ],
-    "Viggo": [
-        "Aston Villa FC", "Burnley FC", "Manchester United FC", "Fulham FC", "Leeds United FC",
-        "Chelsea FC", "Crystal Palace FC", "Brentford FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
-    ],
-    "William": [
-        "Chelsea FC", "AFC Bournemouth", "Manchester City FC", "Fulham FC", "Crystal Palace FC",
-        "West Ham United FC", "Wolverhampton Wanderers FC", "Leeds United FC", "Manchester United FC", "Burnley FC",
-        "Brentford FC", "Nottingham Forest FC", "Sunderland AFC", "Liverpool FC", "Arsenal FC",
-        "Aston Villa FC", "Brighton & Hove Albion FC", "Everton FC", "Tottenham Hotspur FC", "Newcastle United FC"
-    ],
-    "Henrik": [
-        "Wolverhampton Wanderers FC", "Arsenal FC", "Aston Villa FC", "Manchester United FC", "Chelsea FC",
-        "Crystal Palace FC", "Fulham FC", "West Ham United FC", "Brentford FC", "Burnley FC",
-        "Everton FC", "Leeds United FC", "Manchester City FC", "Sunderland AFC", "Tottenham Hotspur FC",
-        "Brighton & Hove Albion FC", "Newcastle United FC", "Nottingham Forest FC", "AFC Bournemouth", "Liverpool FC"
-    ],
-    "Arvid": [
-        "Leeds United FC", "Manchester United FC", "Aston Villa FC", "Chelsea FC", "Crystal Palace FC",
-        "Burnley FC", "Brentford FC", "Fulham FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
-    ],
-    "Vegard": [
-        "Burnley FC", "Aston Villa FC", "Manchester United FC", "Fulham FC", "Leeds United FC",
-        "Chelsea FC", "Crystal Palace FC", "Brentford FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
-    ],
-    "Anders": [
-        "Crystal Palace FC", "Aston Villa FC", "Manchester United FC", "Fulham FC", "Leeds United FC",
-        "Chelsea FC", "Burnley FC", "Brentford FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
+        "Arsenal FC", "Liverpool FC", "Chelsea FC", "Aston Villa FC", "Manchester City FC", "Newcastle United FC", "AFC Bournemouth", "Tottenham Hotspur FC", "Manchester United FC", "Everton FC", "Crystal Palace FC", "Brighton & Hove Albion FC", "Brentford FC", "Fulham FC", "Sunderland AFC", "Nottingham Forest FC", "Leeds United FC", "Burnley FC", "West Ham United FC", "Wolverhampton Wanderers FC"
     ],
     "Christoffer": [
-        "Brentford FC", "Aston Villa FC", "Manchester United FC", "Fulham FC", "Leeds United FC",
-        "Chelsea FC", "Crystal Palace FC", "Burnley FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
+        "Arsenal FC", "Tottenham Hotspur FC", "Liverpool FC", "Chelsea FC", "Manchester United FC", "Manchester City FC", "Newcastle United FC", "Sunderland AFC", "Brighton & Hove Albion FC", "Nottingham Forest FC", "Burnley FC", "Brentford FC", "Aston Villa FC", "Fulham FC", "West Ham United FC", "Crystal Palace FC", "Everton FC", "Leeds United FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
     ],
-    "Mads": [
-        "Fulham FC", "Aston Villa FC", "Manchester United FC", "Brentford FC", "Leeds United FC",
-        "Chelsea FC", "Crystal Palace FC", "Burnley FC", "West Ham United FC", "Sunderland AFC",
-        "Nottingham Forest FC", "Tottenham Hotspur FC", "Liverpool FC", "Manchester City FC", "Brighton & Hove Albion FC",
-        "Arsenal FC", "Newcastle United FC", "Everton FC", "AFC Bournemouth", "Wolverhampton Wanderers FC"
+    "Viggo": [
+        "Arsenal FC", "Liverpool FC", "Manchester City FC", "Chelsea FC", "Manchester United FC", "Newcastle United FC", "Tottenham Hotspur FC", "Aston Villa FC", "Everton FC", "Brighton & Hove Albion FC", "Brentford FC", "Wolverhampton Wanderers FC", "Fulham FC", "Leeds United FC", "Burnley FC", "Nottingham Forest FC", "West Ham United FC", "Sunderland AFC", "Crystal Palace FC", "AFC Bournemouth"
+    ],
+    "Fredrik": [
+        "Arsenal FC", "Manchester City FC", "Liverpool FC", "Chelsea FC", "Tottenham Hotspur FC", "Manchester United FC", "Newcastle United FC", "Aston Villa FC", "Crystal Palace FC", "Nottingham Forest FC", "Fulham FC", "Brighton & Hove Albion FC", "Everton FC", "West Ham United FC", "Brentford FC", "Wolverhampton Wanderers FC", "Burnley FC", "Leeds United FC", "AFC Bournemouth", "Sunderland AFC"
+    ],
+    "Vidar": [
+        "Liverpool FC", "Arsenal FC", "Manchester City FC", "Chelsea FC", "Tottenham Hotspur FC", "Newcastle United FC", "Manchester United FC", "Aston Villa FC", "Everton FC", "Crystal Palace FC", "AFC Bournemouth", "West Ham United FC", "Brighton & Hove Albion FC", "Sunderland AFC", "Nottingham Forest FC", "Wolverhampton Wanderers FC", "Leeds United FC", "Burnley FC", "Fulham FC", "Brentford FC"
+    ],
+    "Eline": [
+        "Liverpool FC", "Arsenal FC", "Manchester City FC", "Chelsea FC", "Newcastle United FC", "Aston Villa FC", "Manchester United FC", "Crystal Palace FC", "Tottenham Hotspur FC", "Brighton & Hove Albion FC", "AFC Bournemouth", "Brentford FC", "Nottingham Forest FC", "Everton FC", "Fulham FC", "Leeds United FC", "West Ham United FC", "Sunderland AFC", "Wolverhampton Wanderers FC", "Burnley FC"
+    ],
+    "Henrik": [
+        "Liverpool FC", "Arsenal FC", "Chelsea FC", "Manchester City FC", "Tottenham Hotspur FC", "Manchester United FC", "Newcastle United FC", "Everton FC", "West Ham United FC", "Brighton & Hove Albion FC", "Sunderland AFC", "Burnley FC", "Leeds United FC", "Brentford FC", "Crystal Palace FC", "AFC Bournemouth", "Nottingham Forest FC", "Aston Villa FC", "Fulham FC", "Wolverhampton Wanderers FC"
+    ],
+    "Anders": [
+        "Liverpool FC", "Arsenal FC", "Chelsea FC", "Manchester City FC", "Newcastle United FC", "Tottenham Hotspur FC", "Everton FC", "Manchester United FC", "Aston Villa FC", "Fulham FC", "Brighton & Hove Albion FC", "Crystal Palace FC", "AFC Bournemouth", "Nottingham Forest FC", "West Ham United FC", "Sunderland AFC", "Leeds United FC", "Brentford FC", "Wolverhampton Wanderers FC", "Burnley FC"
+    ],
+    "William": [
+        "Liverpool FC", "Arsenal FC", "Manchester City FC", "Chelsea FC", "Everton FC", "Tottenham Hotspur FC", "Newcastle United FC", "Brighton & Hove Albion FC", "West Ham United FC", "Manchester United FC", "Crystal Palace FC", "Aston Villa FC", "Brentford FC", "Fulham FC", "Leeds United FC", "AFC Bournemouth", "Nottingham Forest FC", "Sunderland AFC", "Wolverhampton Wanderers FC", "Burnley FC"
     ]
 }
+
